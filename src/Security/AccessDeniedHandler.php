8ee57a55330182ecdc0697d9d8c409ca7f5933f4
Joey Tierney <joeytierney_@hotmail.com>
2018-04-14 19:15:20 +0100

    added twig template for access denied

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -1,15 +1,33 @@
 <?php
 namespace App\Security;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
+    private $twig;
+    private $logger;
+
+    public function __construct(ContainerInterface $container, LoggerInterface $logger)
+    {
+        $this->twig = $container->get('twig');
+        $this->logger = $logger;
+    }
+
+
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
-        return new Response('ACCESS DENIED - You do not have the right permissions to view this page', 403);
+        $this->logger->error('access denied exception');
+
+        $template = 'error/accessDenied.html.twig';
+        $args = [];
+        $html = $this->twig->render($template, $args);
+        return new Response($html);
     }
 }
