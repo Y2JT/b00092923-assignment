da75bc6bbe30a2147cd6a3e26acecd2e761e1b99
Joey Tierney <joeytierney_@hotmail.com>
2018-04-14 18:57:03 +0100

    added AccessDenied handler

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        return new Response('ACCESS DENIED - You do not have the right permissions to view this page', 403);
+    }
+}
