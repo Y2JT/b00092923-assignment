75d0f4e51d7678fb0c0139d5ebfc9b2258dae626
Joey Tierney <joeytierney_@hotmail.com>
2018-04-14 18:28:38 +0100

    added Users table to database, security updates

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,8 +7,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Request;
 
+use App\Entity\Users;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
 class SecurityController extends Controller
 {
+
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
 
     /**
      * @Route("/login", name="login")
@@ -26,6 +41,24 @@ class SecurityController extends Controller
             'last_username' => $lastUsername,
             'error'         => $error,
         ];
+
+        /*
+        if(null != $lastUsername){
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->findOneBy([
+                        'username' => $lastUsername,
+                    ]
+                );
+
+            $plainPassword = 'smith';
+            print $this->encoder->encodePassword($user, $plainPassword);
+
+            var_dump($user);
+            die();
+        }
+        */
+
 
         return $this->render($template, $args);
     }
---
+++ src/DataFixtures/LoadUsers.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\Users;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class LoadUsers extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        // create objects
+        $userUser = $this->createUser('user', 'user');
+        $userAdmin = $this->createUser('admin', 'admin', ['ROLE_ADMIN']);
+        $userMatt = $this->createUser('matt', 'smith', ['ROLE_ADMIN']);
+
+        // store to DB
+        $manager->persist($userUser);
+        $manager->persist($userAdmin);
+        $manager->persist($userMatt);
+        $manager->flush();
+    }
+
+    /**
+     * @param       $username
+     * @param       $plainPassword
+     * @param array $roles // default to ROLE_USER if no ROLE supplied
+     *
+     * @return Users
+     */
+    private function createUser($username, $plainPassword, $roles = ['ROLE_USER']):Users
+    {
+        $user = new Users();
+        $user->setUsername($username);
+        $user->setRoles($roles);
+
+        // password - and encoding
+        $encodedPassword = $this->encodePassword($user, $plainPassword);
+        $user->setPassword($encodedPassword);
+
+        return $user;
+    }
+
+    private function encodePassword($user, $plainPassword):string
+    {
+        $encodedPassword = $this->encoder->encodePassword($user, $plainPassword);
+        return $encodedPassword;
+    }
+}
